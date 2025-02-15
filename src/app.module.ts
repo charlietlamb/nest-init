4a2c01a72d070cd7edff0e27958d10d59dd75611
Charlie Lamb <charlielamb20@icloud.com>
2025-02-15 19:23:49 +0000

    adding cat schema + mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsService } from './cats/cats.service';
 import { CatsController } from './cats/cats.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:nest')],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
